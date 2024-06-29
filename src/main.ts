09fc60e26edce5dca73fcace8cdf6751b8c94302
skyhi-github <wama.skyhi@gmail.com>
2024-06-29 07:43:15 +0700

    first commit

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { registerPlugins } from '@/plugins'
+import App from './App.vue'
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Login from './components/pages/Login.vue';
+import Home from './components/pages/Home.vue';
+import Register from './components/pages/Register.vue'
+import axios from 'axios';
+
+const app = createApp(App)
+registerPlugins(app)
+
+axios.defaults.baseURL = 'http://localhost:3000'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes:[
+        { path: '/', component: Home},
+        { path: '/login', component: Login},
+        { path: '/register', component: Register},
+    ]
+});
+
+app.use(router)
+
+app.mount('#app')
